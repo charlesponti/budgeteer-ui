531a5bd6301f418b8a149b0e1298b211227cdbf8
charlesponti <cj@ponti.io>
2021-06-27 23:03:20 -0700

    remove default columns

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
 
-import { Row, Col } from 'reactstrap';
-
 function Content() {
   return (
-    <div className="my-5">
-      <Row className="d-flex justify-content-between">
-        <Col md={5} className="mb-4">
-          <h6 className="mb-3">
-            Column Three
-          </h6>
-        </Col>
-        <Col md={5} className="mb-4">
-          <h6 className="mb-3">
-            Column Two
-          </h6>
-        </Col>
-        <Col md={5} className="mb-4">
-          <h6 className="mb-3">
-            Column One
-          </h6>
-        </Col>
-      </Row>
-    </div>
+    <div className="my-5" />
   );
 }
 
